14bca78224438a9bf29db208a14f9c511d7fd2d5
Пользователь <umirbayev2004@gmail.com>
2022-12-29 21:51:46 +0500

    Change to js file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let winMiddle = document.querySelector(".list-item-center")
 let winTop = document.querySelectorAll(".list-item-top")
 let wid = 241
 let hei = 226
-let topWin = 111
+// let topWin = 111
 
 function func(){
     if(inpHeight.value == '' && inpWidth.value == ''){
@@ -16,13 +16,12 @@ function func(){
     let diffWidth = +inpWidth.value - wid
     console.log(diffHeight);  
     winSide.forEach(item => {
-        item.setAttribute("style" , `width: ${(+inpWidth.value-15)*0.32}px; height: ${+inpHeight.value * 0.66}px`)
+        item.setAttribute("style" , `width: ${(+inpWidth.value-15)*0.32}px; height: ${(+inpHeight.value-10) * 0.66}px`)
     })
     winTop.forEach(item => {
-        item.setAttribute("style", `width: ${(+inpWidth.value-18)*0.5}px; height: ${+inpHeight.value * 0.34}px`)
-
+        item.setAttribute("style", `width: ${(+inpWidth.value-18)*0.5}px; height: ${(+inpHeight.value-20) * 0.33}px`)
     })
-    winMiddle.setAttribute("style" , `width: ${(+inpWidth.value-15)*0.34}px; height: ${+inpHeight.value * 0.34}px`)
+    winMiddle.setAttribute("style" , `width: ${(+inpWidth.value-15)*0.34}px; height: ${(+inpHeight.value-10) * 0.66}px`)
 
     winBox.setAttribute("style", `height: ${inpHeight.value}px; width: ${inpWidth.value}px`)
 }
